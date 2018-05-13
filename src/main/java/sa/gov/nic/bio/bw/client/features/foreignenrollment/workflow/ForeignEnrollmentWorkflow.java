0c91d8b06745400ecab5b6992fe6dec971ba6826
famalki <falmalki@nic.gov.sa>
2018-05-13 10:40:04 +0300

    improve the workflow engine

--- src/main/java/sa/gov/nic/bio/bw/client/features/foreignenrollment/workflow/ForeignEnrollmentWorkflow.java
+++ src/main/java/sa/gov/nic/bio/bw/client/features/foreignenrollment/workflow/ForeignEnrollmentWorkflow.java
@@ -1,12 +1,8 @@
 package sa.gov.nic.bio.bw.client.features.foreignenrollment.workflow;
 
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import sa.gov.nic.bio.bw.client.core.Context;
 import sa.gov.nic.bio.bw.client.core.interfaces.FormRenderer;
-import sa.gov.nic.bio.bw.client.core.utils.UTF8Control;
 import sa.gov.nic.bio.bw.client.core.workflow.Signal;
-import sa.gov.nic.bio.bw.client.core.workflow.WorkflowBase;
+import sa.gov.nic.bio.bw.client.core.workflow.WizardWorkflowBase;
 import sa.gov.nic.bio.bw.client.features.commons.FaceCapturingFxController;
 import sa.gov.nic.bio.bw.client.features.commons.FingerprintCapturingFxController;
 import sa.gov.nic.bio.bw.client.features.foreignenrollment.ApplicantInfoFxController;
@@ -15,15 +11,11 @@ import sa.gov.nic.bio.bw.client.features.foreignenrollment.LookupFxController;
 import sa.gov.nic.bio.bw.client.features.foreignenrollment.SummaryFxController;
 import sa.gov.nic.bio.bw.client.login.workflow.ServiceResponse;
 
-import java.net.URL;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class ForeignEnrollmentWorkflow extends WorkflowBase<Void, Void>
+public class ForeignEnrollmentWorkflow extends WizardWorkflowBase<Void, Void>
 {
 	public ForeignEnrollmentWorkflow(AtomicReference<FormRenderer> formRenderer,
 	                                 BlockingQueue<Map<String, Object>> userTasks)
@@ -32,110 +24,69 @@ public class ForeignEnrollmentWorkflow extends WorkflowBase<Void, Void>
 	}
 	
 	@Override
-	public Void onProcess(Void input) throws InterruptedException, Signal
+	public void init() throws InterruptedException, Signal
 	{
-		String basePackage = getClass().getPackage().getName().replace(".", "/");
-		basePackage = basePackage.substring(0, basePackage.lastIndexOf('/'));
-		
-		Map<String, Object> uiInputData = new HashMap<>();
-		ResourceBundle stringsBundle;
-		try
-		{
-			stringsBundle = ResourceBundle.getBundle(basePackage + "/bundles/strings",
-			                                         Context.getGuiLanguage().getLocale(), new UTF8Control());
-		}
-		catch(MissingResourceException e)
-		{
-			e.printStackTrace();
-			return null;
-		}
-		
-		URL wizardFxmlLocation = Thread.currentThread().getContextClassLoader()
-				.getResource(basePackage + "/fxml/wizard.fxml");
-		FXMLLoader wizardPaneLoader = new FXMLLoader(wizardFxmlLocation, stringsBundle);
-		wizardPaneLoader.setClassLoader(Context.getFxClassLoader());
-		Context.getCoreFxController().loadWizardBar(wizardPaneLoader);
-		
 		while(true)
 		{
-			while(true)
+			formRenderer.get().renderForm(LookupFxController.class, uiInputData);
+			waitForUserTask();
+			ServiceResponse<Void> serviceResponse = LookupService.execute();
+			if(serviceResponse.isSuccess()) break;
+			else uiInputData.put(KEY_WEBSERVICE_RESPONSE, serviceResponse);
+		}
+	}
+	
+	@Override
+	public Map<String, Object> onStep(int step) throws InterruptedException, Signal
+	{
+		Map<String, Object> uiOutputData = null;
+		
+		switch(step)
+		{
+			case 0:
 			{
-				formRenderer.get().renderForm(LookupFxController.class, uiInputData);
-				waitForUserTask();
-				ServiceResponse<Void> serviceResponse = LookupService.execute();
-				if(serviceResponse.isSuccess()) break;
-				else uiInputData.put(KEY_WEBSERVICE_RESPONSE, serviceResponse);
+				formRenderer.get().renderForm(ApplicantInfoFxController.class, uiInputData);
+				uiOutputData = waitForUserTask();
+				uiInputData.putAll(uiOutputData);
+				break;
 			}
-			
-			uiInputData.clear();
-			int step = 0;
-			
-			while(true)
+			case 1:
 			{
-				Map<String, Object> uiOutputData = null;
-				
-				switch(step)
-				{
-					case 0:
-					{
-						formRenderer.get().renderForm(ApplicantInfoFxController.class, uiInputData);
-						uiOutputData = waitForUserTask();
-						uiInputData.putAll(uiOutputData);
-						break;
-					}
-					case 1:
-					{
-						uiInputData.put(FingerprintCapturingFxController.KEY_HIDE_FINGERPRINT_PREVIOUS_BUTTON,
-						                Boolean.FALSE);
-						formRenderer.get().renderForm(FingerprintCapturingFxController.class, uiInputData);
-						uiOutputData = waitForUserTask();
-						uiInputData.putAll(uiOutputData);
-						break;
-					}
-					case 2:
-					{
-						formRenderer.get().renderForm(FaceCapturingFxController.class, uiInputData);
-						uiOutputData = waitForUserTask();
-						uiInputData.putAll(uiOutputData);
-						break;
-					}
-					case 3:
-					{
-						formRenderer.get().renderForm(SummaryFxController.class, uiInputData);
-						uiOutputData = waitForUserTask();
-						uiInputData.putAll(uiOutputData);
-						break;
-					}
-					case 4:
-					{
-						formRenderer.get().renderForm(DoneFxController.class, uiInputData);
-						uiOutputData = waitForUserTask();
-						uiInputData.putAll(uiOutputData);
-						break;
-					}
-				}
-				
-				if(uiOutputData != null)
-				{
-					Object direction = uiOutputData.get("direction");
-					if("backward".equals(direction))
-					{
-						Platform.runLater(() -> Context.getCoreFxController().moveWizardBackward());
-						step--;
-					}
-					else if("forward".equals(direction))
-					{
-						Platform.runLater(() -> Context.getCoreFxController().moveWizardForward());
-						step++;
-					}
-					else if("startOver".equals(direction))
-					{
-						Platform.runLater(() -> Context.getCoreFxController().moveWizardToTheBeginning());
-						uiInputData.clear();
-						step = 0;
-					}
-				}
+				uiInputData.put(FingerprintCapturingFxController.KEY_HIDE_FINGERPRINT_PREVIOUS_BUTTON,
+				                Boolean.FALSE);
+				formRenderer.get().renderForm(FingerprintCapturingFxController.class, uiInputData);
+				uiOutputData = waitForUserTask();
+				uiInputData.putAll(uiOutputData);
+				break;
+			}
+			case 2:
+			{
+				formRenderer.get().renderForm(FaceCapturingFxController.class, uiInputData);
+				uiOutputData = waitForUserTask();
+				uiInputData.putAll(uiOutputData);
+				break;
+			}
+			case 3:
+			{
+				formRenderer.get().renderForm(SummaryFxController.class, uiInputData);
+				uiOutputData = waitForUserTask();
+				uiInputData.putAll(uiOutputData);
+				break;
+			}
+			case 4:
+			{
+				formRenderer.get().renderForm(DoneFxController.class, uiInputData);
+				uiOutputData = waitForUserTask();
+				uiInputData.putAll(uiOutputData);
+				break;
+			}
+			default:
+			{
+				uiOutputData = waitForUserTask();
+				break;
 			}
 		}
+		
+		return uiOutputData;
 	}
 }
--- src/main/java/sa/gov/nic/bio/bw/client/features/searchbyfaceimage/workflow/SearchByFaceImageWorkflow.java
+++ src/main/java/sa/gov/nic/bio/bw/client/features/searchbyfaceimage/workflow/SearchByFaceImageWorkflow.java
@@ -1,12 +1,8 @@
 package sa.gov.nic.bio.bw.client.features.searchbyfaceimage.workflow;
 
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import sa.gov.nic.bio.bw.client.core.Context;
 import sa.gov.nic.bio.bw.client.core.interfaces.FormRenderer;
-import sa.gov.nic.bio.bw.client.core.utils.UTF8Control;
 import sa.gov.nic.bio.bw.client.core.workflow.Signal;
-import sa.gov.nic.bio.bw.client.core.workflow.WorkflowBase;
+import sa.gov.nic.bio.bw.client.core.workflow.WizardWorkflowBase;
 import sa.gov.nic.bio.bw.client.features.commons.FaceCapturingFxController;
 import sa.gov.nic.bio.bw.client.features.searchbyfaceimage.ConfirmImageFxController;
 import sa.gov.nic.bio.bw.client.features.searchbyfaceimage.ImageSourceFxController;
@@ -16,16 +12,12 @@ import sa.gov.nic.bio.bw.client.features.searchbyfaceimage.UploadImageFileFxCont
 import sa.gov.nic.bio.bw.client.features.searchbyfaceimage.webservice.Candidate;
 import sa.gov.nic.bio.bw.client.login.workflow.ServiceResponse;
 
-import java.net.URL;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class SearchByFaceImageWorkflow extends WorkflowBase<Void, Void>
+public class SearchByFaceImageWorkflow extends WizardWorkflowBase<Void, Void>
 {
 	public static final String KEY_IMAGE_SOURCE = "IMAGE_SOURCE";
 	public static final String KEY_UPLOADED_IMAGE = "UPLOADED_IMAGE";
@@ -42,116 +34,73 @@ public class SearchByFaceImageWorkflow extends WorkflowBase<Void, Void>
 	}
 	
 	@Override
-	public Void onProcess(Void input) throws InterruptedException, Signal
+	public Map<String, Object> onStep(int step) throws InterruptedException, Signal
 	{
-		String basePackage = getClass().getPackage().getName().replace(".", "/");
-		basePackage = basePackage.substring(0, basePackage.lastIndexOf('/'));
+		Map<String, Object> uiOutputData = null;
 		
-		Map<String, Object> uiInputData = new HashMap<>();
-		ResourceBundle stringsBundle;
-		try
+		switch(step)
 		{
-			stringsBundle = ResourceBundle.getBundle(basePackage + "/bundles/strings",
-			                                         Context.getGuiLanguage().getLocale(), new UTF8Control());
-		}
-		catch(MissingResourceException e)
-		{
-			e.printStackTrace();
-			return null;
-		}
-		
-		URL wizardFxmlLocation = Thread.currentThread().getContextClassLoader()
-													   .getResource(basePackage + "/fxml/wizard.fxml");
-		FXMLLoader wizardPaneLoader = new FXMLLoader(wizardFxmlLocation, stringsBundle);
-		Context.getCoreFxController().loadWizardBar(wizardPaneLoader);
-		
-		while(true)
-		{
-			int step = 0;
-			
-			while(true)
+			case 0:
 			{
-				Map<String, Object> uiOutputData = null;
+				formRenderer.get().renderForm(ImageSourceFxController.class, uiInputData);
+				uiOutputData = waitForUserTask();
+				uiInputData.putAll(uiOutputData);
+				break;
+			}
+			case 1:
+			{
+				String imageInput = (String) uiInputData.get(KEY_IMAGE_SOURCE);
 				
-				switch(step)
+				if(VALUE_IMAGE_SOURCE_CAMERA.equals(imageInput))
 				{
-					case 0:
-					{
-						formRenderer.get().renderForm(ImageSourceFxController.class, uiInputData);
-						uiOutputData = waitForUserTask();
-						uiInputData.putAll(uiOutputData);
-						break;
-					}
-					case 1:
-					{
-						String imageInput = (String) uiInputData.get(KEY_IMAGE_SOURCE);
-						
-						if(VALUE_IMAGE_SOURCE_CAMERA.equals(imageInput))
-						{
-							uiInputData.put(FaceCapturingFxController.KEY_ACCEPT_ANY_CAPTURED_IMAGE, Boolean.TRUE);
-							formRenderer.get().renderForm(FaceCapturingFxController.class, uiInputData);
-						}
-						else if(VALUE_IMAGE_SOURCE_UPLOAD.equals(imageInput))
-						{
-							formRenderer.get().renderForm(UploadImageFileFxController.class, uiInputData);
-						}
-						
-						uiOutputData = waitForUserTask();
-						uiInputData.putAll(uiOutputData);
-						break;
-					}
-					case 2:
-					{
-						formRenderer.get().renderForm(ConfirmImageFxController.class, uiInputData);
-						uiOutputData = waitForUserTask();
-						uiInputData.putAll(uiOutputData);
-						break;
-					}
-					case 3:
-					{
-						// show progress indicator here
-						formRenderer.get().renderForm(SearchFxController.class, uiInputData);
-						
-						String imageBase64 = (String) uiInputData.get(SearchByFaceImageWorkflow.KEY_FINAL_IMAGE_BASE64);
-						ServiceResponse<List<Candidate>> response = SearchByFaceImageService.execute(imageBase64);
-						uiInputData.put(KEY_WEBSERVICE_RESPONSE, response);
-						
-						// if success, ask for goNext() automatically. Otherwise, show failure message and retry button
-						formRenderer.get().renderForm(SearchFxController.class, uiInputData);
-						uiOutputData = waitForUserTask();
-						uiInputData.putAll(uiOutputData);
-						break;
-					}
-					case 4:
-					{
-						formRenderer.get().renderForm(ShowResultFxController.class, uiInputData);
-						uiOutputData = waitForUserTask();
-						uiInputData.putAll(uiOutputData);
-						break;
-					}
+					uiInputData.put(FaceCapturingFxController.KEY_ACCEPT_ANY_CAPTURED_IMAGE, Boolean.TRUE);
+					formRenderer.get().renderForm(FaceCapturingFxController.class, uiInputData);
+				}
+				else if(VALUE_IMAGE_SOURCE_UPLOAD.equals(imageInput))
+				{
+					formRenderer.get().renderForm(UploadImageFileFxController.class, uiInputData);
 				}
 				
-				if(uiOutputData != null)
-				{
-					Object direction = uiOutputData.get("direction");
-					if("backward".equals(direction))
-					{
-						Platform.runLater(() -> Context.getCoreFxController().moveWizardBackward());
-						step--;
-					}
-					else if("forward".equals(direction))
-					{
-						Platform.runLater(() -> Context.getCoreFxController().moveWizardForward());
-						step++;
-					}
-					else if("startOver".equals(direction))
-					{
-						Platform.runLater(() -> Context.getCoreFxController().moveWizardToTheBeginning());
-						uiInputData.clear();
-						step = 0;
-					}
-				}
+				uiOutputData = waitForUserTask();
+				uiInputData.putAll(uiOutputData);
+				break;
+			}
+			case 2:
+			{
+				formRenderer.get().renderForm(ConfirmImageFxController.class, uiInputData);
+				uiOutputData = waitForUserTask();
+				uiInputData.putAll(uiOutputData);
+				break;
+			}
+			case 3:
+			{
+				// show progress indicator here
+				formRenderer.get().renderForm(SearchFxController.class, uiInputData);
+				
+				String imageBase64 = (String) uiInputData.get(SearchByFaceImageWorkflow.KEY_FINAL_IMAGE_BASE64);
+				ServiceResponse<List<Candidate>> response = SearchByFaceImageService.execute(imageBase64);
+				uiInputData.put(KEY_WEBSERVICE_RESPONSE, response);
+				
+				// if success, ask for goNext() automatically. Otherwise, show failure message and retry button
+				formRenderer.get().renderForm(SearchFxController.class, uiInputData);
+				uiOutputData = waitForUserTask();
+				uiInputData.putAll(uiOutputData);
+				break;
+			}
+			case 4:
+			{
+				formRenderer.get().renderForm(ShowResultFxController.class, uiInputData);
+				uiOutputData = waitForUserTask();
+				uiInputData.putAll(uiOutputData);
+				break;
+			}
+			default:
+			{
+				uiOutputData = waitForUserTask();
+				break;
 			}
 		}
+		
+		return uiOutputData;
 	}
 }
