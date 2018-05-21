117ed54e0af9980507b7d6b62a5973b1f3d5ee5c
famalki <falmalki@nic.gov.sa>
2018-05-21 16:04:26 +0300

    change visa types lookup to v2

--- src/main/java/sa/gov/nic/bio/bw/client/features/foreignenrollment/webservice/VisaTypeBean.java
+++ src/main/java/sa/gov/nic/bio/bw/client/features/foreignenrollment/webservice/VisaTypeBean.java
@@ -6,15 +6,11 @@ import java.util.Objects;
 public class VisaTypeBean implements Serializable
 {
 	private int code;
-	private String mofaVisaType;
 	private String descriptionAR;
 	private String descriptionEN;
 	
 	public int getCode(){return code;}
 	public void setCode(int code){this.code = code;}
-	
-	public String getMofaVisaType(){return mofaVisaType;}
-	public void setMofaVisaType(String mofaVisaType){this.mofaVisaType = mofaVisaType;}
 	
 	public String getDescriptionAR(){return descriptionAR;}
 	public void setDescriptionAR(String descriptionAR){this.descriptionAR = descriptionAR;}
@@ -28,20 +24,20 @@ public class VisaTypeBean implements Serializable
 		if(this == o) return true;
 		if(o == null || getClass() != o.getClass()) return false;
 		VisaTypeBean that = (VisaTypeBean) o;
-		return code == that.code && Objects.equals(mofaVisaType, that.mofaVisaType) &&
-			   Objects.equals(descriptionAR, that.descriptionAR) && Objects.equals(descriptionEN, that.descriptionEN);
+		return code == that.code && Objects.equals(descriptionAR, that.descriptionAR) &&
+			   Objects.equals(descriptionEN, that.descriptionEN);
 	}
 	
 	@Override
 	public int hashCode()
 	{
-		return Objects.hash(code, mofaVisaType, descriptionAR, descriptionEN);
+		return Objects.hash(code, descriptionAR, descriptionEN);
 	}
 	
 	@Override
 	public String toString()
 	{
-		return "VisaTypeBean{" + "code=" + code + ", mofaVisaType='" + mofaVisaType + '\'' + ", descriptionAR='" +
-			   descriptionAR + '\'' + ", descriptionEN='" + descriptionEN + '\'' + '}';
+		return "VisaTypeBean{" + "code=" + code + ", descriptionAR='" + descriptionAR + '\'' + ", descriptionEN='" +
+			   descriptionEN + '\'' + '}';
 	}
 }
