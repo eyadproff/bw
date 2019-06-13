7108f4fd1bb1a611576ac6b2b23b15782a459b87
nharbi <nharbi@semat.gov.sa>
2019-06-13 14:54:55 +0300

    Change-All-MenuButton-To-ComboBox

--- bw.workflow.biometricsexception/src/main/java/sa/gov/nic/bio/bw/workflow/biometricsexception/controllers/EditMissingFingerprintFXController.java
+++ bw.workflow.biometricsexception/src/main/java/sa/gov/nic/bio/bw/workflow/biometricsexception/controllers/EditMissingFingerprintFXController.java
@@ -65,9 +65,9 @@ public class EditMissingFingerprintFXController extends WizardStepFxControllerBa
     private CheckBox chbLeftLittle;
 
     @FXML
-    private MenuButton MenuRThumb, MenuRIndex, MenuRMiddle, MenuRRing, MenuRLittle;
+    private ComboBox CMenuRThumb, CMenuRIndex, CMenuRMiddle, CMenuRRing, CMenuRLittle;
     @FXML
-    private MenuButton MenuLThumb, MenuLIndex, MenuLMiddle, MenuLRing, MenuLLittle;
+    private ComboBox CMenuLThumb, CMenuLIndex, CMenuLMiddle, CMenuLRing, CMenuLLittle;
 
     @FXML
     private ToggleGroup TGRThumb, TGRIndex, TGRMiddle, TGRRing, TGRLittle;
@@ -82,20 +82,18 @@ public class EditMissingFingerprintFXController extends WizardStepFxControllerBa
 
     @Override
     protected void onAttachedToScene() {
-//        VRightThumb.getChildren().
 
+        AddItemsToMenu(CMenuRThumb, VOtherRThumb);
+        AddItemsToMenu(CMenuRIndex, VOtherRIndex);
+        AddItemsToMenu(CMenuRMiddle, VOtherRMiddle);
+        AddItemsToMenu(CMenuRRing, VOtherRRing);
+        AddItemsToMenu(CMenuRLittle, VOtherRLittle);
 
-        AddItemsToMenu(MenuRThumb, VOtherRThumb);
-        AddItemsToMenu(MenuRIndex, VOtherRIndex);
-        AddItemsToMenu(MenuRMiddle, VOtherRMiddle);
-        AddItemsToMenu(MenuRRing, VOtherRRing);
-        AddItemsToMenu(MenuRLittle, VOtherRLittle);
-
-        AddItemsToMenu(MenuLThumb, VOtherLThumb);
-        AddItemsToMenu(MenuLIndex, VOtherLIndex);
-        AddItemsToMenu(MenuLMiddle, VOtherLMiddle);
-        AddItemsToMenu(MenuLRing, VOtherLRing);
-        AddItemsToMenu(MenuLLittle, VOtherLLittle);
+        AddItemsToMenu(CMenuLThumb, VOtherLThumb);
+        AddItemsToMenu(CMenuLIndex, VOtherLIndex);
+        AddItemsToMenu(CMenuLMiddle, VOtherLMiddle);
+        AddItemsToMenu(CMenuLRing, VOtherLRing);
+        AddItemsToMenu(CMenuLLittle, VOtherLLittle);
 
         if (personfingerprints != null) {
             uploadMissingFPIfAny();
@@ -133,45 +131,32 @@ public class EditMissingFingerprintFXController extends WizardStepFxControllerBa
 
     }
 
-    private void AddItemsToMenu(MenuButton menu, VBox VOther) {
-        MenuItem item = new MenuItem();
-        item.setText(resources.getString("Other"));
-        item.setOnAction(e -> OnActionMenuItemOtherCouse(item.getText(), menu, VOther));
-        menu.getItems().add(item);
+    private void AddItemsToMenu(ComboBox menu, VBox VOther) {
 
-        MenuItem item2 = new MenuItem();
-        item2.setText("جرح");
-        item2.setOnAction(e -> OnActionMenuItem(item2.getText(), menu, VOther));
-        menu.getItems().add(item2);
-
-    }
-
-    @FXML
-    private void OnActionMenuItem(String text, MenuButton menu, VBox VOther) {
-
-        VOther.setVisible(false);
-        menu.setText(text);
+        menu.getItems().addAll(resources.getString("Other"), "جرح");
+        menu.setValue(resources.getString("Cause"));
+        menu.setOnAction(e -> OnActionComboMenu(menu, VOther));
 
 
     }
 
-    @FXML
-    private void OnActionMenuItemOtherCouse(String text, MenuButton menu, VBox VOther) {
-
-        VOther.setVisible(true);
-        menu.setText(text);
+    private void OnActionComboMenu(ComboBox menu, VBox VOther) {
+        if (menu.getValue().toString().equals(resources.getString("Other"))) {
+            VOther.setVisible(true);
+        } else
+            VOther.setVisible(false);
 
     }
 
 
-    private void addMFToPersonFPs(Fingerprint finger, MenuButton Couse, ToggleGroup TG, TextField CouseOther) {
+    private void addMFToPersonFPs(Fingerprint finger, ComboBox Couse, ToggleGroup TG, TextField CouseOther) {
         finger.setMissOrNot(true);
-        if (!Couse.getText().equals(resources.getString("Other"))) {
-            finger.setCouse(Couse.getText());
+        if (!Couse.getValue().toString().equals(resources.getString("Other"))) {
+            finger.setCouse(Couse.getValue().toString());
             finger.setStatus(1);
         } else {
             finger.setCouse(CouseOther.getText());
-            if (((RadioButton) TG.getSelectedToggle()).getText().equals(resources.getString("Temporary")) )
+            if (((RadioButton) TG.getSelectedToggle()).getText().equals(resources.getString("Temporary")))
                 finger.setStatus(1);
             else
                 finger.setStatus(0);
@@ -181,36 +166,36 @@ public class EditMissingFingerprintFXController extends WizardStepFxControllerBa
     private void uploadMissingFPIfAny() {
 
         if (personfingerprints.getRThumb().isMissOrNot())
-            setDataToScene(personfingerprints.getRThumb(), MenuRThumb, TGRThumb, CouseTRThumb, chbRightThumb, VOtherRThumb);
+            setDataToScene(personfingerprints.getRThumb(), CMenuRThumb, TGRThumb, CouseTRThumb, chbRightThumb, VOtherRThumb);
         if (personfingerprints.getRIndex().isMissOrNot())
-            setDataToScene(personfingerprints.getRIndex(), MenuRIndex, TGRIndex, CouseTRIndex, chbRightIndex, VOtherRIndex);
+            setDataToScene(personfingerprints.getRIndex(), CMenuRIndex, TGRIndex, CouseTRIndex, chbRightIndex, VOtherRIndex);
         if (personfingerprints.getRMiddle().isMissOrNot())
-            setDataToScene(personfingerprints.getRMiddle(), MenuRMiddle, TGRMiddle, CouseTRMiddle, chbRightMiddle, VOtherRMiddle);
+            setDataToScene(personfingerprints.getRMiddle(), CMenuRMiddle, TGRMiddle, CouseTRMiddle, chbRightMiddle, VOtherRMiddle);
         if (personfingerprints.getRRing().isMissOrNot())
-            setDataToScene(personfingerprints.getRRing(), MenuRRing, TGRRing, CouseTRRing, chbRightRing, VOtherRRing);
+            setDataToScene(personfingerprints.getRRing(), CMenuRRing, TGRRing, CouseTRRing, chbRightRing, VOtherRRing);
         if (personfingerprints.getRLittle().isMissOrNot())
-            setDataToScene(personfingerprints.getRLittle(), MenuRLittle, TGRLittle, CouseTRLittle, chbRightLittle, VOtherRLittle);
+            setDataToScene(personfingerprints.getRLittle(), CMenuRLittle, TGRLittle, CouseTRLittle, chbRightLittle, VOtherRLittle);
 
         if (personfingerprints.getLThumb().isMissOrNot())
-            setDataToScene(personfingerprints.getLThumb(), MenuLThumb, TGLThumb, CouseTLThumb, chbLeftThumb, VOtherLThumb);
+            setDataToScene(personfingerprints.getLThumb(), CMenuLThumb, TGLThumb, CouseTLThumb, chbLeftThumb, VOtherLThumb);
         if (personfingerprints.getLIndex().isMissOrNot())
-            setDataToScene(personfingerprints.getLIndex(), MenuLIndex, TGLIndex, CouseTLIndex, chbLeftIndex, VOtherLIndex);
+            setDataToScene(personfingerprints.getLIndex(), CMenuLIndex, TGLIndex, CouseTLIndex, chbLeftIndex, VOtherLIndex);
         if (personfingerprints.getLMiddle().isMissOrNot())
-            setDataToScene(personfingerprints.getLMiddle(), MenuLMiddle, TGLMiddle, CouseTLMiddle, chbLeftMiddle, VOtherLMiddle);
+            setDataToScene(personfingerprints.getLMiddle(), CMenuLMiddle, TGLMiddle, CouseTLMiddle, chbLeftMiddle, VOtherLMiddle);
         if (personfingerprints.getLRing().isMissOrNot())
-            setDataToScene(personfingerprints.getLRing(), MenuLRing, TGLRing, CouseTLRing, chbLeftRing, VOtherLRing);
+            setDataToScene(personfingerprints.getLRing(), CMenuLRing, TGLRing, CouseTLRing, chbLeftRing, VOtherLRing);
         if (personfingerprints.getLLittle().isMissOrNot())
-            setDataToScene(personfingerprints.getLLittle(), MenuLLittle, TGLLittle, CouseTLLittle, chbLeftLittle, VOtherLLittle);
+            setDataToScene(personfingerprints.getLLittle(), CMenuLLittle, TGLLittle, CouseTLLittle, chbLeftLittle, VOtherLLittle);
 
 
     }
 
-    private void setDataToScene(Fingerprint finger, MenuButton Couse, ToggleGroup TG, TextField CouseOther, CheckBox chBox, VBox VOther) {
+    private void setDataToScene(Fingerprint finger, ComboBox Couse, ToggleGroup TG, TextField CouseOther, CheckBox chBox, VBox VOther) {
         chBox.setSelected(true);
         if (finger.getCouse().equals("جرح")) {
-            Couse.setText(finger.getCouse());
+            Couse.getSelectionModel().select(finger.getCouse());
         } else {
-            Couse.setText(resources.getString("Other"));
+            Couse.getSelectionModel().select(resources.getString("Other"));
             VOther.setVisible(true);
             if (finger.getStatus() == 0)
                 TG.getToggles().get(0).setSelected(true);
@@ -222,11 +207,11 @@ public class EditMissingFingerprintFXController extends WizardStepFxControllerBa
 
     }
 
-    private boolean isEmpty(MenuButton menu, ToggleGroup TG, TextField Couse) {
-        if (menu.getText().equals(resources.getString("Cause"))) {
+    private boolean isEmpty(ComboBox menu, ToggleGroup TG, TextField Couse) {
+        if (menu.getValue().toString().equals(resources.getString("Cause"))) {
             showWarningNotification(resources.getString("SelectCause"));
             return true;
-        } else if (menu.getText().equals(resources.getString("Other"))) {
+        } else if (menu.getValue().toString().equals(resources.getString("Other"))) {
 
             if (TG.getSelectedToggle() == null) {
                 showWarningNotification(resources.getString("SelectStatus"));
@@ -258,57 +243,57 @@ public class EditMissingFingerprintFXController extends WizardStepFxControllerBa
 
 
         if (chbRightThumb.isSelected()) {
-            if (isEmpty(MenuRThumb, TGRThumb, CouseTRThumb))
+            if (isEmpty(CMenuRThumb, TGRThumb, CouseTRThumb))
                 return;
 
-            addMFToPersonFPs(personfingerprints.getRThumb(), MenuRThumb, TGRThumb, CouseTRThumb);
+            addMFToPersonFPs(personfingerprints.getRThumb(), CMenuRThumb, TGRThumb, CouseTRThumb);
         }
         if (chbRightIndex.isSelected()) {
-            if (isEmpty(MenuRIndex, TGRIndex, CouseTRIndex))
+            if (isEmpty(CMenuRIndex, TGRIndex, CouseTRIndex))
                 return;
-            addMFToPersonFPs(personfingerprints.getRIndex(), MenuRIndex, TGRIndex, CouseTRIndex);
+            addMFToPersonFPs(personfingerprints.getRIndex(), CMenuRIndex, TGRIndex, CouseTRIndex);
         }
         if (chbRightMiddle.isSelected()) {
-            if (isEmpty(MenuRMiddle, TGRMiddle, CouseTRMiddle))
+            if (isEmpty(CMenuRMiddle, TGRMiddle, CouseTRMiddle))
                 return;
-            addMFToPersonFPs(personfingerprints.getRMiddle(), MenuRMiddle, TGRMiddle, CouseTRMiddle);
+            addMFToPersonFPs(personfingerprints.getRMiddle(), CMenuRMiddle, TGRMiddle, CouseTRMiddle);
         }
         if (chbRightRing.isSelected()) {
-            if (isEmpty(MenuRRing, TGRRing, CouseTRRing))
+            if (isEmpty(CMenuRRing, TGRRing, CouseTRRing))
                 return;
-            addMFToPersonFPs(personfingerprints.getRRing(), MenuRRing, TGRRing, CouseTRRing);
+            addMFToPersonFPs(personfingerprints.getRRing(), CMenuRRing, TGRRing, CouseTRRing);
         }
         if (chbRightLittle.isSelected()) {
-            if (isEmpty(MenuRLittle, TGRLittle, CouseTRLittle))
+            if (isEmpty(CMenuRLittle, TGRLittle, CouseTRLittle))
                 return;
-            addMFToPersonFPs(personfingerprints.getRLittle(), MenuRLittle, TGRLittle, CouseTRLittle);
+            addMFToPersonFPs(personfingerprints.getRLittle(), CMenuRLittle, TGRLittle, CouseTRLittle);
         }
 
 
         if (chbLeftThumb.isSelected()) {
-            if (isEmpty(MenuLThumb, TGLThumb, CouseTLThumb))
+            if (isEmpty(CMenuLThumb, TGLThumb, CouseTLThumb))
                 return;
-            addMFToPersonFPs(personfingerprints.getLThumb(), MenuLThumb, TGLThumb, CouseTLThumb);
+            addMFToPersonFPs(personfingerprints.getLThumb(), CMenuLThumb, TGLThumb, CouseTLThumb);
         }
         if (chbLeftIndex.isSelected()) {
-            if (isEmpty(MenuLIndex, TGLIndex, CouseTLIndex))
+            if (isEmpty(CMenuLIndex, TGLIndex, CouseTLIndex))
                 return;
-            addMFToPersonFPs(personfingerprints.getLIndex(), MenuLIndex, TGLIndex, CouseTLIndex);
+            addMFToPersonFPs(personfingerprints.getLIndex(), CMenuLIndex, TGLIndex, CouseTLIndex);
         }
         if (chbLeftMiddle.isSelected()) {
-            if (isEmpty(MenuLMiddle, TGLMiddle, CouseTLMiddle))
+            if (isEmpty(CMenuLMiddle, TGLMiddle, CouseTLMiddle))
                 return;
-            addMFToPersonFPs(personfingerprints.getLMiddle(), MenuLMiddle, TGLMiddle, CouseTLMiddle);
+            addMFToPersonFPs(personfingerprints.getLMiddle(), CMenuLMiddle, TGLMiddle, CouseTLMiddle);
         }
         if (chbLeftRing.isSelected()) {
-            if (isEmpty(MenuLRing, TGLRing, CouseTLRing))
+            if (isEmpty(CMenuLRing, TGLRing, CouseTLRing))
                 return;
-            addMFToPersonFPs(personfingerprints.getLRing(), MenuLRing, TGLRing, CouseTLRing);
+            addMFToPersonFPs(personfingerprints.getLRing(), CMenuLRing, TGLRing, CouseTLRing);
         }
         if (chbLeftLittle.isSelected()) {
-            if (isEmpty(MenuLLittle, TGLLittle, CouseTLLittle))
+            if (isEmpty(CMenuLLittle, TGLLittle, CouseTLLittle))
                 return;
-            addMFToPersonFPs(personfingerprints.getLLittle(), MenuLLittle, TGLLittle, CouseTLLittle);
+            addMFToPersonFPs(personfingerprints.getLLittle(), CMenuLLittle, TGLLittle, CouseTLLittle);
         }
 
         super.onNextButtonClicked(actionEvent);
--- bw.workflow.biometricsexception/src/main/java/sa/gov/nic/bio/bw/workflow/biometricsexception/controllers/FaceExceptionFXController.java
+++ bw.workflow.biometricsexception/src/main/java/sa/gov/nic/bio/bw/workflow/biometricsexception/controllers/FaceExceptionFXController.java
@@ -2,9 +2,7 @@ package sa.gov.nic.bio.bw.workflow.biometricsexception.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import sa.gov.nic.bio.bw.core.controllers.WizardStepFxControllerBase;
 import sa.gov.nic.bio.bw.core.utils.FxmlFile;
@@ -19,7 +17,7 @@ public class FaceExceptionFXController extends WizardStepFxControllerBase {
     @FXML
     private TextField TxtfaceExcReason;
     @FXML
-    private MenuButton SelectCause;
+    private ComboBox ComboMenu;
 
 
     @Override
@@ -32,65 +30,47 @@ public class FaceExceptionFXController extends WizardStepFxControllerBase {
     }
 
     private void AddItemsToMenu() {
-        MenuItem item = new MenuItem();
-        item.setText(resources.getString("Other"));
-        item.setOnAction(e -> OnActionMenuItemOtherCouse(item.getText()));
-        SelectCause.getItems().add(item);
 
-        MenuItem item2 = new MenuItem();
-        item2.setText("جرح");
-        item2.setOnAction(e -> OnActionMenuItem(item2.getText()));
-        //  item2.getGraphic().prefWidth(SelectCause.getWidth());
-        SelectCause.getItems().add(item2);
-    }
-
-    //    private MenuItem prepareMenuItem(String text) {
-//        MenuItem item = new MenuItem();
-//        Label label = new Label();
-//        label.prefWidthProperty().bind(SelectCause.widthProperty());
-//        label.setText(text);
-//        item.setGraphic(label);
-//        item.setOnAction(e -> OnActionMenuItemOtherCouse(item.getText()));
-//        return item;
-//    }
-    private void OnActionMenuItem(String reason) {
-        SelectCause.setText(reason);
-        TxtfaceExcReason.setVisible(false);
-    }
-
-    private void OnActionMenuItemOtherCouse(String reason) {
-
-        SelectCause.setText(resources.getString("Other"));
-        TxtfaceExcReason.setVisible(true);
-
+        ComboMenu.getItems().addAll(resources.getString("Other"), "جرح");
+        ComboMenu.setValue(resources.getString("Cause"));
+        ComboMenu.setOnAction(e -> OnActionComboMenu());
 
     }
+
+    private void OnActionComboMenu() {
+        if (ComboMenu.getValue().toString().equals(resources.getString("Other"))) {
+            TxtfaceExcReason.setVisible(true);
+        } else
+            TxtfaceExcReason.setVisible(false);
+
+    }
+
 
     private void uploadReason() {
-        for (MenuItem item : SelectCause.getItems()) {
-            if (item.getText().equals(Reason)) {
-                SelectCause.setText(Reason);
+        for (Object item : ComboMenu.getItems()) {
+            if (item.toString().equals(Reason)) {
+                ComboMenu.getSelectionModel().select(Reason);
                 return;
             }
 
         }
-        SelectCause.setText(resources.getString("Other"));
+        ComboMenu.getSelectionModel().select(resources.getString("Other"));
         TxtfaceExcReason.setVisible(true);
         TxtfaceExcReason.setText(Reason);
     }
 
     @Override
     protected void onNextButtonClicked(ActionEvent actionEvent) {
-        if (SelectCause.getText().equals(resources.getString("Cause"))) {
+        if (ComboMenu.getValue().toString().equals(resources.getString("Cause"))) {
             showWarningNotification(resources.getString("SelectCause"));
             return;
         }
-        if (SelectCause.getText().equals(resources.getString("Other"))) {
+        if (ComboMenu.getValue().toString().equals(resources.getString("Other"))) {
             if (TxtfaceExcReason.getText().trim().isEmpty()) {
                 showWarningNotification(resources.getString("WriteCause"));
                 return;
             } else Reason = TxtfaceExcReason.getText();
-        } else Reason = SelectCause.getText();
+        } else Reason = ComboMenu.getValue().toString();
 
 
         super.onNextButtonClicked(actionEvent);
