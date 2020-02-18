a7c0ba897c5e8a16ef366e7e5803ee774ccc2eb0
Fouad Almalki <falmalki@semat.gov.sa>
2020-02-18 11:54:14 +0300

    intial impl of latent-reverse

---
+++ bw.workflow.latentreversesearch/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module bw.workflow.latentreversesearch
+{
+	requires bw.core;
+	requires bw.workflow.commons;
+	requires bio.commons;
+	
+	//opens sa.gov.nic.bio.bw.workflow.latentreversesearch.bundles;
+	//opens sa.gov.nic.bio.bw.workflow.latentreversesearch to bw.core;
+	//opens sa.gov.nic.bio.bw.workflow.latentreversesearch.fxml to bw.core;
+	//opens sa.gov.nic.bio.bw.workflow.latentreversesearch.tasks to bw.core;
+	//opens sa.gov.nic.bio.bw.workflow.latentreversesearch.controllers to javafx.fxml, bw.core;
+	//opens sa.gov.nic.bio.bw.workflow.latentreversesearch.beans to org.apache.commons.lang3, com.google.gson;
+}
