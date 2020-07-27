0fa5829ae90e15630c3620bfbe5552310f21d502
nharbi <nharbi@semat.gov.sa>
2020-07-27 12:13:21 +0300

    faceverification-done

---
+++ bw.workflow.biometricsverification/src/main/java/sa/gov/nic/bio/bw/workflow/biometricsverification/BiometricsVerificationWorkflow.java
@@ -0,0 +1,181 @@
+package sa.gov.nic.bio.bw.workflow.biometricsverification;
+
+import sa.gov.nic.bio.bw.core.biokit.FingerPosition;
+import sa.gov.nic.bio.bw.core.utils.Device;
+import sa.gov.nic.bio.bw.core.wizard.Step;
+import sa.gov.nic.bio.bw.core.wizard.Wizard;
+import sa.gov.nic.bio.bw.core.workflow.AssociatedMenu;
+import sa.gov.nic.bio.bw.core.workflow.Signal;
+import sa.gov.nic.bio.bw.core.workflow.WithLookups;
+import sa.gov.nic.bio.bw.core.workflow.WizardWorkflowBase;
+import sa.gov.nic.bio.bw.workflow.biometricsverification.controllers.VerificationMethodSelectionFxController;
+import sa.gov.nic.bio.bw.workflow.biometricsverification.controllers.VerificationMethodSelectionFxController.VerificationMethod;
+import sa.gov.nic.bio.bw.workflow.biometricsverification.controllers.VerificationProgressPaneFxController;
+import sa.gov.nic.bio.bw.workflow.commons.controllers.FaceCapturingFxController;
+import sa.gov.nic.bio.bw.workflow.commons.controllers.SingleFingerprintCapturingFxController;
+import sa.gov.nic.bio.bw.workflow.commons.tasks.FingerprintVerificationWorkflowTask;
+import sa.gov.nic.bio.bw.workflow.faceverification.beans.FaceMatchingResponse;
+import sa.gov.nic.bio.bw.workflow.faceverification.controllers.MatchingFxController;
+import sa.gov.nic.bio.bw.workflow.faceverification.controllers.PersonIdPaneFxController;
+import sa.gov.nic.bio.bw.workflow.biometricsverification.controllers.ShowResultFxController;
+import sa.gov.nic.bio.bw.workflow.faceverification.tasks.FaceVerificationWorkflowTask;
+import sa.gov.nic.bio.bw.workflow.commons.lookups.CountriesLookup;
+import sa.gov.nic.bio.bw.workflow.searchbyfaceimage.controllers.ConfirmImageFxController;
+import sa.gov.nic.bio.bw.workflow.searchbyfaceimage.controllers.ImageSourceFxController;
+import sa.gov.nic.bio.bw.workflow.searchbyfaceimage.controllers.ImageSourceFxController.Source;
+import sa.gov.nic.bio.bw.workflow.searchbyfaceimage.controllers.UploadImageFileFxController;
+
+@AssociatedMenu(workflowId = 1028, menuId = "menu.query.biometricsVerification", menuTitle = "menu.title", menuOrder = 11,
+                devices = {Device.FINGERPRINT_SCANNER, Device.CAMERA, Device.IRIS_SCANNER})
+@WithLookups(CountriesLookup.class)
+//@Wizard({@Step(iconId = "\\uf2bb", title = "wizard.InquiryByPersonId"),
+//               // @Step(iconId = "database", title = "wizard.inquiryResult"),
+//                @Step(iconId = "\\uf256", title = "wizard.fingerprintCapturing"),
+//                @Step(iconId = "camera", title = "wizard.facePhotoCapturing"),
+//                @Step(iconId = "th_list", title = "wizard.reviewAndSubmit"),
+//                @Step(iconId = "spinner", title = "wizard.enrollment"),
+//                @Step(iconId = "\\uf0f6", title = "wizard.enrollmentResult")})
+@Wizard({@Step(iconId = "\\uf2bb", title = "wizard.personId"),
+                @Step(iconId = "question", title = "wizard.selectVerificationMethod"),
+                 @Step(iconId = "question", title = "wizard.imageSource"),
+             //   @Step(iconId = "question", title = "wizard.imageSource"),
+                @Step(iconId = "upload", title = "wizard.uploadImage"),
+                @Step(iconId = "unlock", title = "wizard.confirm"),
+                @Step(iconId = "\\uf248", title = "wizard.matching"),
+                @Step(iconId = "file_text_alt", title = "wizard.showResult")})
+public class BiometricsVerificationWorkflow extends WizardWorkflowBase {
+    @Override
+    public void onStep(int step) throws InterruptedException, Signal {
+
+        switch (step) {
+            case 0: {
+                renderUiAndWaitForUserInput(PersonIdPaneFxController.class);
+                break;
+            }
+            case 1: {
+                renderUiAndWaitForUserInput(VerificationMethodSelectionFxController.class);
+                break;
+            }
+            case 2: {
+                VerificationMethod verificationMethod = getData(VerificationMethodSelectionFxController.class,
+                        "verificationMethod");
+                if (verificationMethod == VerificationMethod.FACE_PHOTO) {
+                    renderUiAndWaitForUserInput(ImageSourceFxController.class);
+                }
+                else if (verificationMethod == VerificationMethod.FINGERPRINT) {
+                    incrementNSteps(2); // to skip steps #3 #4 on going next
+                    setData(SingleFingerprintCapturingFxController.class, "acceptBadQualityFingerprint",
+                            Boolean.TRUE);
+                    setData(SingleFingerprintCapturingFxController.class, "acceptBadQualityFingerprintMinRetires",
+                            0);
+                    renderUiAndWaitForUserInput(SingleFingerprintCapturingFxController.class);
+                }
+                else {
+
+                }
+
+                break;
+            }
+            case 3: {
+
+                Source imageSource = getData(ImageSourceFxController.class, "imageSource");
+
+                if (Source.UPLOAD.equals(imageSource)) {
+                    renderUiAndWaitForUserInput(UploadImageFileFxController.class);
+                }
+                else if (Source.CAMERA.equals(imageSource)) {
+                    setData(FaceCapturingFxController.class, "acceptAnyCapturedImage", true);
+                    renderUiAndWaitForUserInput(FaceCapturingFxController.class);
+                }
+
+                break;
+            }
+            case 4: {
+
+                Source imageSource = getData(ImageSourceFxController.class, "imageSource");
+
+                passData(PersonIdPaneFxController.class, ConfirmImageFxController.class,
+                        "personId");
+
+                if (Source.UPLOAD.equals(imageSource)) {
+                    passData(UploadImageFileFxController.class, "uploadedImage",
+                            ConfirmImageFxController.class, "facePhoto");
+                }
+                else if (Source.CAMERA.equals(imageSource)) {
+                    passData(FaceCapturingFxController.class, ConfirmImageFxController.class,
+                            "facePhoto");
+                }
+
+                renderUiAndWaitForUserInput(ConfirmImageFxController.class);
+                break;
+            }
+            case 5: {
+//                renderUiAndWaitForUserInput(MatchingFxController.class);
+//
+//                passData(PersonIdPaneFxController.class, FaceVerificationWorkflowTask.class,
+//                        "personId");
+//                passData(ConfirmImageFxController.class, FaceVerificationWorkflowTask.class,
+//                        "facePhotoBase64");
+//                executeWorkflowTask(FaceVerificationWorkflowTask.class);
+//
+////
+                VerificationMethod verificationMethod = getData(VerificationMethodSelectionFxController.class,
+                        "verificationMethod");
+
+                setData(VerificationProgressPaneFxController.class, "verificationMethod", verificationMethod);
+
+                renderUiAndWaitForUserInput(VerificationProgressPaneFxController.class);
+
+                if(VerificationMethod.FINGERPRINT.equals(verificationMethod))
+                {
+                    incrementNSteps(-2); // to skip steps #3 #4 on going previous
+                    FingerPosition selectedFingerprintPosition = getData(SingleFingerprintCapturingFxController.class,
+                            "selectedFingerprintPosition");
+
+                    passData(PersonIdPaneFxController.class, FingerprintVerificationWorkflowTask.class,
+                            "personId");
+                    passData(SingleFingerprintCapturingFxController.class, "capturedFingerprintForBackend",
+                            FingerprintVerificationWorkflowTask.class, "fingerprint");
+                    setData(FingerprintVerificationWorkflowTask.class, "fingerPosition",
+                            selectedFingerprintPosition.getPosition());
+
+                    executeWorkflowTask(FingerprintVerificationWorkflowTask.class);
+
+                    passData(FingerprintVerificationWorkflowTask.class, VerificationProgressPaneFxController.class,
+                            "matched");
+                }
+                else if(VerificationMethod.FACE_PHOTO.equals(verificationMethod))
+                {
+                    passData(PersonIdPaneFxController.class, FaceVerificationWorkflowTask.class,
+                            "personId");
+                    passData(ConfirmImageFxController.class, FaceVerificationWorkflowTask.class,
+                            "facePhotoBase64");
+
+                    executeWorkflowTask(FaceVerificationWorkflowTask.class);
+
+                    FaceMatchingResponse faceMatchingResponse= getData(FaceVerificationWorkflowTask.class,
+                            "faceMatchingResponse");
+                    setData(VerificationProgressPaneFxController.class,
+                            "matched",faceMatchingResponse.isMatched());
+                }
+
+                break;
+            }
+            case 6: {
+                passData(PersonIdPaneFxController.class, ShowResultFxController.class,
+                        "personId");
+                passData(ConfirmImageFxController.class, ShowResultFxController.class,
+                        "facePhoto");
+                passData(FaceVerificationWorkflowTask.class, ShowResultFxController.class,
+                        "faceMatchingResponse");
+
+                renderUiAndWaitForUserInput(ShowResultFxController.class);
+                break;
+            }
+        }
+
+    }
+}
+
+
+
---
+++ bw.workflow.biometricsverification/src/main/java/sa/gov/nic/bio/bw/workflow/biometricsverification/controllers/VerificationMethodSelectionFxController.java
@@ -0,0 +1,138 @@
+package sa.gov.nic.bio.bw.workflow.biometricsverification.controllers;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import sa.gov.nic.bio.bw.core.Context;
+import sa.gov.nic.bio.bw.core.controllers.WizardStepFxControllerBase;
+import sa.gov.nic.bio.bw.core.utils.FxmlFile;
+import sa.gov.nic.bio.bw.core.workflow.Output;
+
+import java.util.Map;
+
+@FxmlFile("verificationMethod.fxml")
+public class VerificationMethodSelectionFxController extends WizardStepFxControllerBase {
+    public enum VerificationMethod {
+        FINGERPRINT,
+        FACE_PHOTO
+    }
+
+    @Output private VerificationMethod verificationMethod;
+
+    @FXML private RadioButton rbByFingerprint;
+    @FXML private RadioButton rbByFacePhoto;
+    @FXML private Button btnPrevious;
+    @FXML private Button btnNext;
+
+    private boolean minusOneStep = false;
+
+    @Override
+    protected void onAttachedToScene() {
+
+        // load the old state, if exists
+        if (VerificationMethod.FINGERPRINT.equals(verificationMethod)) {
+            rbByFingerprint.setSelected(true);
+            rbByFingerprint.requestFocus();
+            minusOneStep = true;
+        }
+        else {
+            rbByFacePhoto.setSelected(true);
+            rbByFacePhoto.requestFocus();
+        }
+
+
+        System.out.println(minusOneStep + " test");
+        // go next on pressing ENTER on the radio buttons
+        EventHandler<KeyEvent> eventHandler = event ->
+        {
+            if (event.getCode() == KeyCode.ENTER) {
+                btnNext.fire();
+                event.consume();
+            }
+        };
+        rbByFingerprint.addEventHandler(KeyEvent.KEY_PRESSED, eventHandler);
+        rbByFacePhoto.addEventHandler(KeyEvent.KEY_PRESSED, eventHandler);
+
+
+        String fingerprintCapturingTitle = resources.getString("wizard.singleFingerprintCapturing");
+        String facePhotoSourceTitle = resources.getString("wizard.imageSource");
+
+        // change the wizard-step-indicator upon changing the verification method
+        int stepIndex = Context.getCoreFxController().getWizardPane(getTabIndex())
+                .getStepIndexByTitle(fingerprintCapturingTitle);
+        if (stepIndex < 0) {
+            stepIndex = Context.getCoreFxController().getWizardPane(getTabIndex())
+                    .getStepIndexByTitle(facePhotoSourceTitle);
+        }
+
+        final int finalStepIndex = stepIndex;
+
+        rbByFingerprint.selectedProperty().addListener((observable, oldValue, newValue) ->
+        {
+            if (newValue) {
+
+                Context.getCoreFxController()
+                        .getWizardPane(getTabIndex()).updateStep(finalStepIndex,
+                        fingerprintCapturingTitle,
+                        "\\uf25a");
+
+                if (!minusOneStep) {
+                    Context.getCoreFxController().getWizardPane(getTabIndex()).removeStep(finalStepIndex + 1);
+                    Context.getCoreFxController().getWizardPane(getTabIndex()).removeStep(finalStepIndex + 1);
+                    minusOneStep = true;
+                }
+            }
+
+        });
+
+        rbByFacePhoto.selectedProperty().addListener((observable, oldValue, newValue) ->
+        {
+            if (newValue) {
+                Context.getCoreFxController()
+                        .getWizardPane(getTabIndex()).updateStep(finalStepIndex, facePhotoSourceTitle,
+                        "question");
+
+                if (minusOneStep) {
+                    Context.getCoreFxController()
+                            .getWizardPane(getTabIndex())
+                            .addStep(finalStepIndex + 1, resources.getString("wizard.uploadImage"),
+                                    "upload");
+                    Context.getCoreFxController()
+                            .getWizardPane(getTabIndex()).addStep(finalStepIndex + 2, resources.getString("wizard" +
+                                                                                                          ".confirm"),
+                            "unlock");
+                    minusOneStep = false;
+                }
+                else {
+                    Context.getCoreFxController()
+                            .getWizardPane(getTabIndex())
+                            .updateStep(finalStepIndex + 1, resources.getString("wizard.uploadImage"),
+                                    "upload");
+                    Context.getCoreFxController()
+                            .getWizardPane(getTabIndex()).updateStep(finalStepIndex + 2, resources.getString(
+                            "wizard.confirm"),
+                            "unlock");
+
+                }
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    protected void onGoingPrevious(Map<String, Object> uiDataMap) {
+        onGoingNext(uiDataMap);
+    }
+
+    @Override
+    public void onGoingNext(Map<String, Object> uiDataMap) {
+        if (rbByFacePhoto.isSelected()) { verificationMethod = VerificationMethod.FACE_PHOTO; }
+        else { verificationMethod = VerificationMethod.FINGERPRINT; }
+    }
+}
